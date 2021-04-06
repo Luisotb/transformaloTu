b532ab869d50864c6d7e65764694a20e02e19e3f
root <root@Equipo-702.localdomain>
2021-04-06 12:25:55 +0200

    initial commit

---
+++ src/main/java/com/transformalotu/transformalotu/TransformalotuApplication.java
@@ -0,0 +1,13 @@
+package com.transformalotu.transformalotu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformalotuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformalotuApplication.class, args);
+	}
+
+}
