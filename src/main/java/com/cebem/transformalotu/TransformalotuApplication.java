8cd4c2d2a7ec9adf55907eaca2f0fe4eb137dfd8
kant003 <gonzalezm.angel@gmail.com>
2021-04-08 13:48:00 +0200

    corrigiendo la estructura del proyecto

--- src/main/java/com/cebem/transformalotu/TransformalotuApplication.java
+++ src/main/java/com/cebem/transformalotu/TransformalotuApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.cebem.transformalotu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TransformalotuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TransformalotuApplication.class, args);
 	}
 
 }
--- src/test/java/com/cebem/transformalotu/TransformalotuApplicationTests.java
+++ src/test/java/com/cebem/transformalotu/TransformalotuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.cebem.transformalotu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TransformalotuApplicationTests {
 
 	@Test
 	void contextLoads() {
