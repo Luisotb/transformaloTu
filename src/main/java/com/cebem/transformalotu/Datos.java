b2d1884fd49a881ed1f40a91642a71390c54345d
abelgarcia2 <abelgg2000@gmail.com>
2021-04-14 22:51:06 +0200

    Subo el codigo que completa la caracteristica #7

--- src/main/java/com/cebem/transformalotu/Datos.java
+++ src/main/java/com/cebem/transformalotu/Datos.java
@@ -12,5 +12,18 @@ public class Datos {
     String toUpperCase(){
         return texto.toUpperCase();
     }
+    
+    public boolean buscarPalabra(String palabra){
+        String[] txt = texto.split(" ");
+        boolean encontrado = false;
+
+        for(int i=0;i<txt.length;i++){
+            if(txt[i].equals(palabra)){
+                encontrado = true;
+            }
+        }
+        
+        return encontrado;
+    }
 
 }
