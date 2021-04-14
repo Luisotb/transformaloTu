ed192071c9ec0678db0675e3d98e2020ea37c43f
manuchei <manueldominguezgallego@gmail.com>
2021-04-14 13:01:27 +0200

    Añado codigo feature 4

---
+++ src/main/java/com/cebem/transformalotu/Contar_Caracteres.java
@@ -0,0 +1,19 @@
+package com.cebem.transformalotu;
+
+public class Contar_Caracteres {
+   public static void main(String[] args) {
+
+    String frase = "Hola trabajo en Cebem";
+
+    int stringLength = frase.length();
+
+    System.out.println("Esta frase tiene: " +stringLength+ " caracteres ");
+
+   }
+}
+
+
+
+
+
+
