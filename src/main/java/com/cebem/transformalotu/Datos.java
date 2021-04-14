fa5f675818eb67441cbbfdc11671cdc4a8bce54c
Yari <yariana894@gmail.com>
2021-04-14 12:52:17 +0200

    "subo código para completar feature05"

--- src/main/java/com/cebem/transformalotu/Datos.java
+++ src/main/java/com/cebem/transformalotu/Datos.java
@@ -8,7 +8,7 @@ public class Datos {
         texto = "Cebem";
         numeros = new int[]{1,2,3};
     }
-    
+
     Strint [] MuestraCaracterLinea(){
         for(int i = 0;i<texto.length();i++){
         }
