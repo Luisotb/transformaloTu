85df04d92b9874e87bc0bbf2762aa99797522371
samuelalonsogil <asbarxas.samuelalonso@gmail.com>
2021-04-19 10:40:10 +0200

    vuelvo a subir mi parte a ver si de esta va

--- src/main/java/com/cebem/transformalotu/Datos.java
+++ src/main/java/com/cebem/transformalotu/Datos.java
@@ -3,34 +3,22 @@ package com.cebem.transformalotu;
 import java.util.Scanner;
 
 public class Datos {
-    static Scanner sc = new Scanner(System.in); 
+    static Scanner sc = new Scanner(System.in);
     String texto;
-    
-    public static String IntroducirTexto(String texto){
-        System.out.println("Introduce el texto: ");
-        return texto = sc.nextLine();
-    }
 
-    public static char IntroducirLetra(char letra){
-        System.out.println("Introduce la letra a buscar: ");
-        return letra = sc.nextLine().charAt(0);
-    }
-
-    public static String BuscarLetra(String texto,char letra){
-        String posiciones="";
-        for(int i = 0;i<=texto.length();i++){
-            if (texto.charAt(i)==letra) {
+    public static String buscarLetra(String texto, char letra) {
+        String posiciones = "";
+        for (int i = 0; i <= texto.length(); i++) {
+            if (texto.charAt(i) == letra) {
                 posiciones = posiciones + texto.charAt(i) + " , ";
             }
         }
-        return "La letra se encuentra en las posiciones: " + posiciones;
+        return  posiciones;
     }
 
-    public Datos(){
+    public Datos() {
         texto = "Cebem";
-        
-    }
 
-    
+    }
 
 }
