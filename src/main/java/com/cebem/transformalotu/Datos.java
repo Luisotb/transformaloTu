18b93b848e6b676037dd9e4f3dee8715f279a8ed
Ángel Barreiro Pisos <ligtus6@gmail.com>
2021-04-21 14:17:30 +0200

    Revert "Añadido código de feature #09"
    This reverts commit bb35c802d6cf893ea44d0c648a57ef12f326b9a4.

--- src/main/java/com/cebem/transformalotu/Datos.java
+++ src/main/java/com/cebem/transformalotu/Datos.java
@@ -3,130 +3,112 @@ package com.cebem.transformalotu;
 import java.util.Scanner;
 
 public class Datos {
-	static Scanner sc = new Scanner(System.in);
-	String texto;
-	int[] datos;
+    static Scanner sc = new Scanner(System.in);
+    String texto;
+    int[] datos;
+    public static String buscarLetra(String texto, char letra) {
+        String posiciones = "";
+        for (int i = 0; i <= texto.length(); i++) {
+            if (texto.charAt(i) == letra) {
+                posiciones = posiciones + texto.charAt(i) + " , ";
+            }
+        }
+        return  posiciones;
+    }
 
-	public static String buscarLetra(String texto, char letra) {
-		String posiciones = "";
-		for (int i = 0; i <= texto.length(); i++) {
-			if (texto.charAt(i) == letra) {
-				posiciones = posiciones + texto.charAt(i) + " , ";
-			}
-		}
-		return posiciones;
-	}
+    public Datos() {
+        texto = "Cebem";
+        datos = new int[] {1,2,3};
+    }
 
-	public Datos() {
-		texto = "Cebem";
-		datos = new int[] { 1, 2, 3 };
-	}
+    /**
+     * Analiza si una palabra está o no en un texto.
+     * @param palabra La palabra que queremos buscar
+     * @return true si la palabra está en el texto, false en caso contrario
+     */
+    public boolean buscarPalabra(String palabra, String text){
+        String[] txt = text.split(" ");
+        boolean encontrado = false;
 
-	/**
-	 * Analiza si una palabra está o no en un texto.
-	 * 
-	 * @param palabra La palabra que queremos buscar
-	 * @return true si la palabra está en el texto, false en caso contrario
-	 */
-	public boolean buscarPalabra(String palabra, String text) {
-		String[] txt = text.split(" ");
-		boolean encontrado = false;
+        for(int i=0;i<txt.length;i++){
+            if(txt[i].equals(palabra)){
+                encontrado = true;
+            }
+        }
+        
+        return encontrado;
+    }
 
-		for (int i = 0; i < txt.length; i++) {
-			if (txt[i].equals(palabra)) {
-				encontrado = true;
-			}
-		}
+    String invierteTexto() {
+        String finalText = "";
+        int lText = texto.length();
 
-		return encontrado;
-	}
+        for (int i = lText - 1; i >= 0; i--) {
+            finalText.concat(texto.charAt(i)+"");
+        }
+        return finalText;
+    }
 
-	String invierteTexto() {
-		String finalText = "";
-		int lText = texto.length();
+    public String borrarCaracteres(String deleteChar){
+        return texto.replace(deleteChar, "");
+    }
 
-		for (int i = lText - 1; i >= 0; i--) {
-			finalText.concat(texto.charAt(i) + "");
-		}
-		return finalText;
-	}
+    void invertirOrdenDatos() {
+        int aux;
+        for (int i = 0; i < datos.length / 2; i++) {
+            aux = datos[i];
+            datos[i] = datos[datos.length - 1 - i];
+            datos[datos.length - 1 - i] = aux;
+        }
+    }
 
-	public String borrarCaracteres(String deleteChar) {
-		return texto.replace(deleteChar, "");
-	}
 
-	void invertirOrdenDatos() {
-		int aux;
-		for (int i = 0; i < datos.length / 2; i++) {
-			aux = datos[i];
-			datos[i] = datos[datos.length - 1 - i];
-			datos[datos.length - 1 - i] = aux;
-		}
-	}
 
-	public String espaciosPorGuiones() {
-		String palabr = texto.replaceAll(" ", "-");
-		return palabr;
-	}
+    public String espaciosPorGuiones(){
+        String palabr = texto.replaceAll(" ", "-");
+        return palabr;
+    }
 
-	public static String alternateCaps(String text) {
-		boolean caps = true;
-		String result = "";
-		String[] words = text.split(" ");
-		for (int i = 0; i < words.length; i++) {
-			String word = "";
-			for (int j = 0; j < words[i].length(); j++) {
-				if (caps) {
-					word += words[i].toUpperCase().charAt(j);
-					caps = false;
-				} else {
-					word += words[i].toLowerCase().charAt(j);
-					caps = true;
-				}
-			}
-			words[i] = word + " ";
-			result += words[i];
-		}
-		return result;
-	}
 
-	public static String muestraCaracterEnLinea(String texto) {
+
+
+ public static String muestraCaracterEnLinea(String texto) {
 		String aux = "";
 		for (int i = 0; i < texto.length(); i++) {
-			aux += texto.charAt(i) + "\n";
+			aux+= texto.charAt(i)+"\n";
 		}
 		return aux;
 	}
 
-	public String mayusculas(String toUpperCase) {
-		String texto = "";
-		return texto.toUpperCase();
-	}
+    public String mayusculas(String toUpperCase){
+        String texto = "";
+        return texto.toUpperCase();
+    }
 
-	String sustituirVocalesPorI(String cadena) {
-		/**
-		 * Este metodo recibe una cadena y la transforma sustituyendo todas las vocales
-		 * por la vocal i. Esta sustitucion es sensitive case.
-		 */
-		// definicion de varibles
-		String cadenaVocalesI = "";
-		char caracter;
+    String sustituirVocalesPorI(String cadena) {
+        /**
+        * Este metodo recibe una cadena y la transforma sustituyendo todas las vocales
+        * por la vocal i. Esta sustitucion es sensitive case.
+        */
+       // definicion de varibles
+       String cadenaVocalesI = "";
+       char caracter;
 
-		// Bucle para recorrer la cadena caracter a caracter
-		for (int i = 0; i < cadena.length(); i++) {
-			caracter = cadena.charAt(i);
-			// Condición para sustituir por 'i'
-			if (caracter == 'a' || caracter == 'e' || caracter == 'o' || caracter == 'u') {
-				cadenaVocalesI += 'i';
-				// Condición para sustituir por 'I'
-			} else if (caracter == 'A' || caracter == 'E' || caracter == 'O' || caracter == 'U') {
-				cadenaVocalesI += 'I';
-				// En caso de que no secumpla nada no se modifica el carácter
-			} else {
-				cadenaVocalesI += caracter;
-			}
-		}
-		return cadenaVocalesI;
-	}
+       // Bucle para recorrer la cadena caracter a caracter
+       for(int i=0; i<cadena.length(); i++) {
+           caracter = cadena.charAt(i);
+           // Condición para sustituir por 'i'
+           if(caracter == 'a' || caracter == 'e' || caracter == 'o' || caracter == 'u') {
+               cadenaVocalesI += 'i';
+           // Condición para sustituir por 'I'
+           }else if(caracter == 'A' || caracter == 'E' || caracter == 'O' || caracter == 'U') {
+               cadenaVocalesI += 'I';
+           // En caso de que no secumpla nada no se modifica el carácter
+           }else {
+               cadenaVocalesI += caracter;
+           }
+       }
+       return cadenaVocalesI;
+   }
 
 }
