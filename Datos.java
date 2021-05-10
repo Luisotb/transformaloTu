72386f9672af4316b75a1925f3c1855f7d6b28ea
APerezVila <perezvilaAlicia@gmail.com>
2021-05-10 11:36:42 +0200

    Subo el código editado para la feature 004

--- Datos.java
+++ Datos.java
@@ -3,11 +3,11 @@ import java.util.regex.Pattern;
 import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 public class Datos {
-    public static int contarCaracteres(String text){
+    public int contarCaracteres(){
         int caracteresEncontrados = 0;
         String regEx = "^[a-zA-Z0-9]$";
-        for(int i = 0; i<text.length();i++){
-            if(Pattern.matches(regEx, text.substring(i,i+1))){
+        for(int i = 0; i<texto.length();i++){
+            if(Pattern.matches(regEx, texto.substring(i,i+1))){
                 caracteresEncontrados+=1;
             }
             
